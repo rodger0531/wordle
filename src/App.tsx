35472b4a14236f659a5e04f476b223560f6f2262
Rodger <rodger0531@gmail.com>
2022-02-01 21:35:34 +0800

    create dictionary and import file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import * as R from "ramda";
+import dictionary from "./Asset/array";
 
 function App() {
+  useEffect(() => {
+    console.log(dictionary);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Test</header>
     </div>
   );
 }
