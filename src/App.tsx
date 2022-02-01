7aa43168f100d74d4e88691142e3a413cd165a7b
Rodger <rodger0531@gmail.com>
2022-02-01 22:26:15 +0800

    add keyboard event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, KeyboardEvent, useState } from "react";
 import "./App.css";
 import * as R from "ramda";
 import dictionary from "./Asset/array";
 
+const isAllowedKey = (key: string): boolean =>
+  key.slice(0, 3) === "Key" || key === "Enter" || key === "Backspace";
+
 function App() {
   useEffect(() => {
-    console.log(dictionary);
+    // console.log(dictionary);
   }, []);
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (!e.repeat && isAllowedKey(e.code)) {
+      if (e.code === "Enter" || e.code === "Backspace") {
+        return;
+      }
+      console.log("test", e.code.slice(3));
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">Test</header>
+      <div className="App-header" tabIndex={-1} onKeyDown={handleKeyPress}>
+        Test
+      </div>
     </div>
   );
 }
