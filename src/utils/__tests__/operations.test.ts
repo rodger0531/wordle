b928be2828b34601cf6a2f0daade17274f725bd5
rodger0531 <rodger0531@gmail.com>
2022-07-22 23:39:59 +0800

    tests: processGuess

--- src/utils/__tests__/operations.test.ts
+++ src/utils/__tests__/operations.test.ts
@@ -1,4 +1,4 @@
-import { isAllowedKey } from "../operations";
+import { isAllowedKey, processGuess } from "../operations";
 
 describe("isAllowedKey test", () => {
   it("Key should return true", () => {
@@ -19,4 +19,14 @@ describe("isAllowedKey test", () => {
   it("Backspace1 should return false", () => {
     expect(isAllowedKey("Backspace1")).toBe(false);
   });
+});
+
+test("processGuess", () => {
+  expect(processGuess("TESTS", "BESTS")).toEqual([0, 2, 2, 2, 2]);
+  expect(processGuess("PARTY", "STAIN")).toEqual([0, 1, 0, 1, 0]);
+  expect(processGuess("STONE", "ALONE")).toEqual([0, 0, 2, 2, 2]);
+  expect(processGuess("STATS", "ATLAS")).toEqual([0, 2, 1, 0, 2]);
+  expect(processGuess("BEAST", "ROCKY")).toEqual([0, 0, 0, 0, 0]);
+  expect(processGuess("TASAT", "STATA")).toEqual([1, 1, 1, 1, 1]);
+  expect(processGuess("STAIN", "STAIN")).toEqual([2, 2, 2, 2, 2]);
 });
